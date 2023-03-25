ca6f16c7e0caf2a2e1444192823f6e530241938d
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-25 17:59:46 +0700

    update-NFT-title

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -2,10 +2,27 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { SIZES, FONTS, COLORS, SHADOWS, assets } from "../constants";
 
-export const NFTTitle = () => {
+export const NFTTitle = ({ title, subTitle, titleSize, subtitleSize }) => {
     return (
         <View>
-            <Text>NFTTitle</Text>
+            <Text
+                style={{
+                    fontFamily: FONTS.semiBold,
+                    fontSize: titleSize,
+                    color: COLORS.primary,
+                }}
+            >
+                {title}
+            </Text>
+            <Text
+                style={{
+                    fontFamily: FONTS.regular,
+                    fontSize: subtitleSize,
+                    color: COLORS.primary,
+                }}
+            >
+                {subTitle}
+            </Text>
         </View>
     );
 };
