2937666789cd15af290f21c914246726c739db64
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-28 09:27:34 +0700

    update-DetailsDesc

--- screens/Details.js
+++ screens/Details.js
@@ -5,26 +5,33 @@ import { COLORS, SIZES, assets, SHADOWS, FONTS } from "../constants";
 import { CircleButton, RectButton, SubInfo, DetailsDesc, DetailsBid, FocusedStatusBar } from "../components";
 
 const DetailsHeader = ({ data, navigation }) => (
-  <View style={{ width: "100%", height: 373 }}>
-    <Image
-      source={data.image}
-      resizeMode="cover"
-      style={{ width: "100%", height: "100%" }}
-    />
+  <>
+    <View
+      style={{
+        width: "100%",
+        height: 373
+      }}
+    >
+      <Image
+        source={data.image}
+        resizeMode="cover"
+        style={{ width: "100%", height: "100%" }}
+      />
 
-    <CircleButton
-      imgUrl={assets.left}
-      handlePress={() => navigation.goBack()}
-      left={15}
-      top={StatusBar.currentHeight + 10}
-    />
+      <CircleButton
+        imgUrl={assets.left}
+        handlePress={() => navigation.goBack()}
+        left={15}
+        top={StatusBar.currentHeight + 10}
+      />
 
-    <CircleButton
-      imgUrl={assets.heart}
-      right={15}
-      top={StatusBar.currentHeight + 10}
-    />
-  </View>
+      <CircleButton
+        imgUrl={assets.heart}
+        right={15}
+        top={StatusBar.currentHeight + 10}
+      />
+    </View>
+  </>
 );
 
 const Details = ({ route, navigation }) => {
