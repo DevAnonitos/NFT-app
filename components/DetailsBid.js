92039efd9d5ee3ad35742adbf54501ae61399e66
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-03-29 09:33:47 +0700

    update-DetailsBid

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -1,15 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { EthPrice } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
 
-const DetailsBid = () => {
+const DetailsBid = ({ bid }) => {
     return (
         <>
             <View style={{
-
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems:  'center',
+                marginVertical: SIZES.base,
+                paddingHorizontal: SIZES.base * 2,
             }}>
-                <Text>
-                    DetailsBid
-                </Text>
+                <Image
+                    source={bid.image}
+                    resizeMode='contain'
+                    style={{
+                        width: 48,
+                        height: 48,
+                    }}
+                />
+
+                <View
+                    style={{
+                        flex: 1,
+                        alignItems: "center",
+                        justifyContent: 'center',
+                        paddingHorizontal: SIZES.base,
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontFamily: FONTS.semiBold,
+                            fontSize: SIZES.small,
+                            color: COLORS.primary,
+                        }}
+                    >
+                        Bid placed by {bid.name}
+                    </Text>
+                    <Text
+                        style={{
+                            fontFamily: FONTS.regular,
+                            fontSize: SIZES.small -2,
+                            color: COLORS.secondary,
+                            marginTop: 3,
+                        }}
+                    >
+                        {bid.date}
+                    </Text>
+                </View>
+
+                <EthPrice
+                    price={bid.price}
+                />
             </View>
         </>
     );
